41ba28ee6f123a2c4f35f1e0441340442e7aaf07
shivakumar2006 <official.shivakumar06@gmail.com>
2025-01-27 14:19:47 +0530

    commit

--- src/components/CryptoCurrencies.jsx
+++ src/components/CryptoCurrencies.jsx
@@ -33,7 +33,7 @@ const CryptoCurrencies = ({ simplified }) => {
         )}
         
         <Row gutter={[32, 32]} className='crypto-card-container'>
-            {cryptos ?.map((currency) => (
+            {cryptos?.map((currency) => (
                 <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
                     <Link to={`/crypto/${currency.uuid}`}>
                         <Card title={`${currency.rank}. ${currency.name}`}
